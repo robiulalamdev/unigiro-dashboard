26640a615aa9774332d1a5af1917c3c8a1ce8cc7
robiulalamdev <robiulalam762400@gmail.com>
2024-01-04 16:28:57 +0600

    initiale page add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,18 @@
 import Image from "next/image";
 import { Manrope } from "next/font/google";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
 export default function Home() {
+  const { pathname, push } = useRouter();
+  // console.log(pathname);
+  useEffect(() => {
+    if (pathname === "/") {
+      push("/overview");
+    }
+  }, [pathname]);
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${manrope.className}`}
